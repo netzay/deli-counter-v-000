ca3da4537a502faee4b03c68d20f590ae60c654e
Ally Netzel <netzelally@gmail.com>
2018-03-31 00:38:25 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.any? == false
+    puts "The line is currently empty."
+  else
+    spots = ["The line is currently:"]
+    katz_deli.each_with_index do |spot, index|
+      spots << "#{index+1}. #{spot}"
+    end
+    puts spots.join(" ")
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+end
